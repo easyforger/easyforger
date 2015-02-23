6997dc654eec9df00ba6d1b266f38cf773e99280
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-02-22 23:53:35 -0300

    add helpers for dye colors

--- src/main/scala/com/easyforger/base/EasyForger.scala
+++ src/main/scala/com/easyforger/base/EasyForger.scala
@@ -4,4 +4,4 @@ import com.easyforger.chests.VanillaChests
 import com.easyforger.creatures.VanillaCreatures
 import com.easyforger.dungeons.VanillaDungeons
 
-trait EasyForger extends ItemStackConversions with VanillaChests with VanillaDungeons with VanillaCreatures
+trait EasyForger extends VanillaItems with VanillaChests with VanillaDungeons with VanillaCreatures
---
+++ src/main/scala/com/easyforger/base/VanillaItems.scala
@@ -0,0 +1,27 @@
+package com.easyforger.base
+
+import net.minecraft.block.Block
+import net.minecraft.init.Items._
+import net.minecraft.item.{Item, ItemStack}
+
+trait VanillaItems {
+  implicit def toItemStack(item: Item): ItemStack = new ItemStack(item, 1)
+  implicit def toItemStack(block: Block): ItemStack = new ItemStack(block, 1)
+
+  val blackDye = new ItemStack(dye, 1, 0)
+  val redDye = new ItemStack(dye, 1, 1)
+  val greenDye = new ItemStack(dye, 1, 2)
+  val brownDye = new ItemStack(dye, 1, 3)
+  val blueDye = new ItemStack(dye, 1, 4)
+  val purpleDye = new ItemStack(dye, 1, 5)
+  val cyanDye = new ItemStack(dye, 1, 6)
+  val silverDye = new ItemStack(dye, 1, 7)
+  val grayDye = new ItemStack(dye, 1, 8)
+  val pinkDye = new ItemStack(dye, 1, 9)
+  val limeDye = new ItemStack(dye, 1, 10)
+  val yellowDye = new ItemStack(dye, 1, 11)
+  val lightBlueDye = new ItemStack(dye, 1, 12)
+  val magentaDye = new ItemStack(dye, 1, 13)
+  val orangeDye = new ItemStack(dye, 1, 14)
+  val whiteDye = new ItemStack(dye, 1, 15)
+}
