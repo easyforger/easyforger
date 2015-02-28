0c93987db51f8774d035422c13080412c64a14db
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-02-28 19:41:15 -0300

    add item creation helpers

---
+++ src/main/scala/sample/mods/items/ItemChestKey.scala
@@ -0,0 +1,5 @@
+package sample.mods.items
+
+import com.easyforger.items.EasyForgerItem
+
+class ItemChestKey extends EasyForgerItem(ItemsMod.modId, "chestkey", List("yellow", "red", "blue"))
---
+++ src/main/scala/sample/mods/items/ItemsMod.scala
@@ -0,0 +1,16 @@
+package sample.mods.items
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.registry.GameRegistry
+
+@Mod(modid = ItemsMod.modId, name = "EasyForger Items Sample Mod", version = "0.1", modLanguage = "scala")
+object ItemsMod {
+  final val modId = "easyforger_items"
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent) = {
+    GameRegistry.registerItem(new ItemChestKey(), "ChestKey")
+  }
+}
