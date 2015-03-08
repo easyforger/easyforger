e4e88f5a00073567c945d40e41f301ddcec92787
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-03-07 22:46:57 -0300

    recipe for the cloth block

--- src/main/scala/sample/mods/blocks/BlocksMod.scala
+++ src/main/scala/sample/mods/blocks/BlocksMod.scala
@@ -1,16 +1,29 @@
 package sample.mods.blocks
 
+import com.easyforger.recipes._
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import cpw.mods.fml.common.registry.GameRegistry
+import net.minecraft.init.Items
 
 @Mod(modid = BlocksMod.modId, name = "EasyForger Blocks Sample Mod", version = "0.1", modLanguage = "scala")
 object BlocksMod {
   final val modId = "easyforger_blocks"
-  
+
+  val cloth = new BlockCloth()
+
   @EventHandler
   def preInit(event: FMLPreInitializationEvent) = {
-    GameRegistry.registerBlock(new BlockCloth(), "BlockCloth")
+    GameRegistry.registerBlock(cloth, "BlockCloth")
+
+    crafting(
+      Items.string to cloth withShape
+        """
+          |sss
+          |s.s
+          |sss
+        """.stripMargin
+    )
   }
 }
