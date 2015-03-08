a0335f49e775e39db7756a56612dd8bd84037e4d
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-03-07 23:20:38 -0300

    fix unlocalized names for item armors and add i18n for the lava suit

--- src/main/scala/com/easyforger/items/EasyForgerItemArmor.scala
+++ src/main/scala/com/easyforger/items/EasyForgerItemArmor.scala
@@ -6,12 +6,14 @@ import net.minecraft.item.{ItemArmor, ItemStack}
 
 class EasyForgerItemArmor(val modId: String, material: ArmorMaterial, armorType: ArmorType, val subItemsNames: List[String] = Nil)
   extends ItemArmor(material, 0, armorType.id) with ItemCommon {
-  
+
+  setUnlocalizedName(s"${modId}_${armorType.name}")
+
   lazy val name = s"${material.name()}_${armorType.name}"
 
   override def getArmorTexture(stack: ItemStack, entity: Entity, slot: Int, subType: String): String = {
-    val texture1 = List(s"${modId}_${material.name()}_helmet", s"${modId}_${material.name()}_chestplate", s"${modId}_${material.name()}_boots")
-    val texture2 = List(s"${modId}_${material.name()}_leggings")
+    val texture1 = List(s"${modId}_helmet", s"${modId}_chestplate", s"${modId}_boots")
+    val texture2 = List(s"${modId}_leggings")
 
     if (texture1.exists(stack.getItem.getUnlocalizedName.contains(_))) s"$modId:textures/models/armor/${material.name()}_layer_1.png"
     else if (texture2.exists(stack.getItem.getUnlocalizedName.contains(_))) s"$modId:textures/models/armor/${material.name()}_layer_2.png"
