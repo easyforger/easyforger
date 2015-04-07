a9966ca7ea7ca555b5de91ba22749f410786089e
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-04-06 23:13:39 -0300

    remove all sample mods (they now belong to their own project)

--- src/main/scala/sample/mods/items/ItemBanana.scala
+++
@@ -1,9 +0,0 @@
-package sample.mods.items
-
-import com.easyforger.items.EasyForgerItemFood
-import net.minecraft.potion.Potion
-
-class ItemBanana extends EasyForgerItemFood(ItemsMod.modId, "banana", 5, 0.4f, false) {
-  setPotionEffect(Potion.jump.id, 9, 1, 1f)
-  addPotionEffect(Potion.digSlowdown.id, 5, 0, 0.5f)
-}
--- src/main/scala/sample/mods/items/ItemChestKey.scala
+++
@@ -1,8 +0,0 @@
-package sample.mods.items
-
-import com.easyforger.items.EasyForgerItem
-import net.minecraft.creativetab.CreativeTabs
-
-class ItemChestKey extends EasyForgerItem(ItemsMod.modId, "chestkey", List("yellow", "red", "blue")) {
-  setCreativeTab(CreativeTabs.tabMisc)
-}
