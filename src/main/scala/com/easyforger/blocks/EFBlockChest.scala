928ba5e3fac17205ec2a54d572a186d9c32e6836
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-05-10 00:09:04 -0300

    #24 allows specifying the type of chest (normal or trapped) in EFBlockChest closes #24

--- src/main/scala/com/easyforger/blocks/EFBlockChest.scala
+++ src/main/scala/com/easyforger/blocks/EFBlockChest.scala
@@ -1,6 +1,14 @@
 package com.easyforger.blocks
 
+import com.easyforger.blocks.ChestType.ChestType
 import net.minecraft.block.BlockChest
 
-class EFBlockChest(val modId: String, val name: String)
-  extends BlockChest(0) with BlockCommon
+class EFBlockChest(val modId: String, val name: String, chestType: ChestType)
+  extends BlockChest(chestType.id) with BlockCommon
+
+object ChestType extends Enumeration {
+  type ChestType = Value
+
+  val Normal = Value(0)
+  val Trapped = Value(1)
+}
