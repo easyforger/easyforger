657cb480196a6eb33a809a3767023188f028a7f9
JCranky <paulo.siqueira@gmail.com>
2016-02-21 15:45:22 +0100

    add scalacheck, and license headers to all source files

--- src/main/scala/com/easyforger/blocks/BlockCommon.scala
+++ src/main/scala/com/easyforger/blocks/BlockCommon.scala
@@ -1,3 +1,7 @@
+/*
+ * This file is part of EasyForger which is released under GPLv3 License.
+ * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl-3.0.en.html for full license details.
+ */
 package com.easyforger.blocks
 
 import java.util.Random
