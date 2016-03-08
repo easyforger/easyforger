dfa5728fa2de60e1d25cc1bcc3d6d2966650c156
JCranky <paulo.siqueira@gmail.com>
2016-03-08 22:34:42 +0100

    Enable ScalaStyle VarFieldChecker rule as a warning
    This also changes EFItemFood to use a val ListBuffer instead of a var List
    for the extra potion effects.

--- src/main/scala/com/easyforger/items/EFItemFood.scala
+++ src/main/scala/com/easyforger/items/EFItemFood.scala
@@ -9,15 +9,16 @@ import net.minecraft.item.{ItemFood, ItemStack}
 import net.minecraft.potion.PotionEffect
 import net.minecraft.world.World
 
-class EFItemFood(val modId: String, val name: String, food: Int, saturation: Float, wolfFood: Boolean,
-                         val subItemsNames: List[String] = Nil)
+import scala.collection.mutable.ListBuffer
+
+class EFItemFood(val modId: String, val name: String, food: Int, saturation: Float, wolfFood: Boolean, val subItemsNames: List[String] = Nil)
   extends ItemFood(food, saturation, wolfFood) with ItemCommon {
 
-  // store the creation function, need a new object every time the potion is going to be applied
-  private var extraPotionEffects = List.empty[(() => PotionEffect, Float)]
+  // store the creation function, a new PotionEffect object is needed every time the potion is going to be applied
+  private val extraPotionEffects = ListBuffer.empty[(() => PotionEffect, Float)]
 
   def addPotionEffect(potionId: Int, duration: Int, level: Int, probability: Float): Unit = {
-    extraPotionEffects = extraPotionEffects :+ (() => new PotionEffect(potionId, duration * 20, level)) -> probability
+    extraPotionEffects += (() => new PotionEffect(potionId, duration * 20, level)) -> probability
   }
 
   override def onFoodEaten(itemStack: ItemStack, world: World, player: EntityPlayer): Unit = {
